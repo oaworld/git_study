94d9e12e5abf1816b9f199e0e620203af7bd864f
leeyunho <lovesky21s@naver.com>
2022-08-08 18:25:41 +0900

    test5 master commit 1

--- 20220808/test5.php
+++ 20220808/test5.php
@@ -26,6 +26,24 @@ class Test extends MY_Controller {
     }
 
     public function get_user_list() {
-     
+         // Todo 타이틀
+         if (is_value_format_valid('todo_title', $todoTitle) == false)
+         $this->response(array('response_code'=>self::HTTP_BAD_REQUEST, 'response_message'=>$this->lang->line('todo_title_invalid')), $this->http_header_redefine['HTTP_BAD_REQUEST']);
+        // 반복 유형이 있을 경우
+        if (isset($scheduleType)) {
+            // 대문자로 변경
+            $scheduleType = strtoupper($scheduleType);
+            // 스케쥴 타입 상세 정보
+            $scheduleTypeList = array(
+                'D'=>'day', // 매일
+                'WD'=>'day', // 평일
+                'W'=>'week', // 매주
+                'M'=>'month', // 매월
+                'Y'=>'year' // 매년
+            );
+            // 존재하지 않는 유형
+            if (!array_key_exists($scheduleType, $scheduleTypeList))
+                $this->response(array('response_code'=>self::HTTP_BAD_REQUEST, 'response_message'=>$this->lang->line('schedule_type_invalid')), $this->http_header_redefine['HTTP_BAD_REQUEST']);
+        }      
     }
 }
