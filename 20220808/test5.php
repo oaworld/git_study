14dc68d297d6e516cf14ebec6f91130ec729cb8e
leeyunho <lovesky21s@naver.com>
2022-08-08 19:02:51 +0900

    m1

--- 20220808/test5.php
+++ 20220808/test5.php
@@ -27,6 +27,9 @@ class Test extends MY_Controller {
     public function mmm() {
         
     }
+    public function mmm1() {
+        
+    }
 
     public function test4_branch_1() {
         //오아데이 확인 - (추가) 2020/07/31 오아데이일 경우 상품상세페이지에서 레드클럽 혜택 영역 비노출 처리 (오아데이에는 레드클럽 혜택 미적용한다)
